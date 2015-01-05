f37a513f9d5bf3393af1b5404a0d2378aa34658e
Vladimir Kostyukov <vkostyukov@twitter.com>
2015-01-05 15:01:11 -0800

    Draft version of the serial client/server

--- serial-core/src/main/scala/io/github/finagle/Serial.scala
+++ serial-core/src/main/scala/io/github/finagle/Serial.scala
@@ -3,23 +3,48 @@ package io.github.finagle
 import java.net.SocketAddress
 
 import com.twitter.finagle._
-import io.github.finagle.serial.{DeserializeReq, SerializeRep}
+import com.twitter.finagle.mux.{Response, Request}
+import com.twitter.io.Buf
+import com.twitter.util.{Return, Future}
+import io.github.finagle.serial.{Deserialize, Serialize}
+
+import scala.util.Success
 
 /**
  * A Serial Mux server and client.
  *
- * @param serialize the serialization function
- * @param deserialize the deserialization function
  * @tparam Req the request type
  * @tparam Rep the response type
  */
 class Serial[Req, Rep](
-  serialize: SerializeRep[Rep],
-  deserialize: DeserializeReq[Req]
+  serializeReq: Serialize[Req],
+  serializeRep: Serialize[Rep],
+  deserializeReq: Deserialize[Req],
+  deserializeRep: Deserialize[Rep]
 ) extends Server[Req, Rep] with Client[Req, Rep] {
 
-  override def serve(addr: SocketAddress, service: ServiceFactory[Req, Rep]): ListeningServer = ???
-  override def newClient(dest: Name, label: String): ServiceFactory[Req, Rep] = ???
+  private val muxToObject = new Filter[mux.Request, mux.Response, Req, Rep] {
+    override def apply(muxReq: mux.Request, service: Service[Req, Rep]): Future[mux.Response] = {
+      Future.const(deserializeReq(Buf.ByteArray.Owned.extract(muxReq.body))) flatMap service map { rep =>
+        mux.Response(Buf.ByteArray.Owned(serializeRep(rep)))
+      }
+    }
+  }
+
+  override def serve(addr: SocketAddress, factory: ServiceFactory[Req, Rep]): ListeningServer =
+    Mux.server.serve(addr, muxToObject andThen factory)
+
+  override def newClient(dest: Name, label: String): ServiceFactory[Req, Rep] =
+    Mux.client.newClient(dest, label) map { service =>
+      new Service[Req, Rep] {
+        override def apply(req: Req): Future[Rep] = {
+          val muxReq = mux.Request(Path.empty, Buf.ByteArray.Owned(serializeReq(req)))
+          service(muxReq) flatMap { muxRep =>
+            Future.const(deserializeRep(Buf.ByteArray.Owned.extract(muxRep.body)))
+          }
+        }
+      }
+    }
 }
 
 /**
@@ -32,6 +57,7 @@ class Serial[Req, Rep](
  * }}}
  */
 object Serial {
-  def apply[Req, Rep](implicit serialize: SerializeRep[Rep], deserialize: DeserializeReq[Req]) =
-    new Serial[Req, Rep](serialize, deserialize)
+  def apply[Req, Rep](implicit serializeReq: Serialize[Req], serializeRep: Serialize[Rep],
+                      deserializeReq: Deserialize[Req], deserializeRep: Deserialize[Rep]) =
+    new Serial[Req, Rep](serializeReq, serializeRep, deserializeReq, deserializeRep)
 }
--- serial-core/src/main/scala/io/github/finagle/serial/package.scala
+++ serial-core/src/main/scala/io/github/finagle/serial/package.scala
@@ -2,46 +2,46 @@ package io.github.finagle
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
 
-import scala.util.{Failure, Success, Try}
+import com.twitter.util.{Throw, Return, Try}
 
 package object serial {
 
   /**
-   * An abstraction that serializes the responses.
+   * An abstraction that serializes the object ''o''.
    *
-   * @tparam A the response type
+   * @tparam A the object type
    */
-  trait SerializeRep[-A] {
-    def apply(rep: A): Array[Byte]
+  trait Serialize[-A] {
+    def apply(o: A): Array[Byte]
   }
 
   /**
-   * An abstraction that deserializes the requests.
+   * An abstraction that deserializes the array ''a''.
    *
-   * @tparam A the request type
+   * @tparam A the object type
    */
-  trait DeserializeReq[+A] {
-    def apply(req: Array[Byte]): Try[A]
+  trait Deserialize[+A] {
+    def apply(a: Array[Byte]): Try[A]
   }
 
-  def serializeWithJavaIO[A] = new SerializeRep[A] {
-    override def apply(rep: A): Array[Byte] = {
+  def serializeWithJavaIO[A] = new Serialize[A] {
+    override def apply(req: A): Array[Byte] = {
       val byteArray = new ByteArrayOutputStream()
       val out = new ObjectOutputStream(byteArray)
-      try { out.writeObject(rep) }
+      try { out.writeObject(req) }
       finally { out.close() }
 
       byteArray.toByteArray
     }
   }
 
-  def deserializeWithJavaIO[A] = new DeserializeReq[A] {
-    override def apply(req: Array[Byte]): Try[A] = {
-      val byteArray = new ByteArrayInputStream(req)
+  def deserializeWithJavaIO[A] = new Deserialize[A] {
+    override def apply(rep: Array[Byte]): Try[A] = {
+      val byteArray = new ByteArrayInputStream(rep)
       val in = new ObjectInputStream(byteArray)
 
-      try { Success(in.readObject().asInstanceOf[A]) }
-      catch { case e: Exception  => Failure(e) }
+      try { Return(in.readObject().asInstanceOf[A]) }
+      catch { case e: Exception  => Throw(e) }
       finally { in.close() }
     }
   }
