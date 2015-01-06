33aa0593123334a4336feec1ff6b3c296ac23e55
Vladimir Kostyukov <vkostyukov@twitter.com>
2015-01-06 14:23:57 -0800

    Add initial support for pickling

--- serial-pickling/src/main/scala/io/github/finagle/serial/pickling/package.scala
+++ serial-pickling/src/main/scala/io/github/finagle/serial/pickling/package.scala
@@ -1,5 +1,17 @@
 package io.github.finagle.serial
 
-package object pickling {
+import com.twitter.util.{Throw, Return, Try}
 
+import scala.pickling._
+import scala.pickling.binary._
+
+package object pickling {
+  implicit def toPickling[A: SPickler: Unpickler: FastTypeTag]: Codec[A] = new Codec[A] {
+    override val serialize = new Serialize[A] {
+      override def apply(o: A): Try[Array[Byte]] = Return(o.pickle.value)
+    }
+    override val deserialize = new Deserialize[A] {
+      override def apply(a: Array[Byte]): Try[A] = Throw(new UnsupportedOperationException)
+    }
+  }
 }
