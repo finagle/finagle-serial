15afaff1466cd6b8658c1abdc6c2b2ea92ac1fc5
Vladimir Kostyukov <vkostyukov@twitter.com>
2015-01-06 13:59:04 -0800

    Add tests

--- serial-core/src/test/scala/io/github/finagle/SerialSpec.scala
+++ serial-core/src/test/scala/io/github/finagle/SerialSpec.scala
@@ -19,21 +19,53 @@ class SerialSpec extends FlatSpec with Matchers {
 
     override val deserialize = new Deserialize[String] {
       override def apply(a: Array[Byte]) = a match {
-        case Array(0xDE, 0xAD, 0xBE, 0xEF) => Throw(new IllegalArgumentException)
+        case Array(102, 111, 111) => // "foo"
+          Throw(new IllegalArgumentException)
         case _ => Return(new String(a))
       }
     }
   }
 
-  "Serial" should "work as expected with no errors in codec" in {
-    val server = Serial[String, String].serve(
-      new InetSocketAddress(8123),
-      new Service[String, String] {
-        override def apply(req: String) = Future.value(req.reverse)
-      })
+  def mockServer = Serial[String, String].serve(
+    new InetSocketAddress(8123),
+    new Service[String, String] {
+      override def apply(req: String) = Future.value(req.reverse)
+    })
 
-    val service = Serial[String, String].newService("localhost:8123")
+  def mockService = Serial[String, String].newService("localhost:8123")
+
+  "Serial" should "work as expected with no errors in serialization codec" in {
+    val server = mockServer
+    val service = mockService
     Await.result(service("abcd")) shouldBe "dcba"
+    Await.ready(server.close())
+  }
+
+  it should "results in future exception if the request serialization failed" in {
+    val server = mockServer
+    val service = mockService
+    an [IllegalArgumentException] should be thrownBy Await.result(service("dead beef"))
+    Await.ready(server.close())
+  }
+
+  ignore should "results in future exception if the response serialization failed" in {
+    val server = mockServer
+    val service = mockService
+    an [IllegalArgumentException] should be thrownBy Await.result(service("feeb daed"))
+    Await.ready(server.close())
+  }
+
+  ignore should "results in future exception if request deserialization failed" in {
+    val server = mockServer
+    val service = mockService
+    an [IllegalArgumentException] should be thrownBy Await.result(service("foo"))
+    Await.ready(server.close())
+  }
+
+  it should "results in future exception if response deserialization failed" in {
+    val server = mockServer
+    val service = mockService
+    an [IllegalArgumentException] should be thrownBy Await.result(service("oof"))
     Await.ready(server.close())
   }
 }
