d24aefb27924a09a6343156b5db75b0e7cbda30a
Vladimir Kostyukov <vkostyukov@twitter.com>
2015-01-06 17:46:53 -0800

    Fix #3

--- serial-pickling/src/main/scala/io/github/finagle/serial/pickling/package.scala
+++ serial-pickling/src/main/scala/io/github/finagle/serial/pickling/package.scala
@@ -8,10 +8,14 @@ import scala.pickling.binary._
 package object pickling {
   implicit def toPickling[A: SPickler: Unpickler: FastTypeTag]: Codec[A] = new Codec[A] {
     override val serialize = new Serialize[A] {
-      override def apply(o: A): Try[Array[Byte]] = Return(o.pickle.value)
+      override def apply(o: A): Try[Array[Byte]] =
+        try { Return(o.pickle.value) }
+        catch { case e: Exception => Throw(e) }
     }
     override val deserialize = new Deserialize[A] {
-      override def apply(a: Array[Byte]): Try[A] = Return(a.unpickle[A])
+      override def apply(a: Array[Byte]): Try[A] =
+        try { Return(a.unpickle[A]) }
+        catch { case e: Exception => Throw(e) }
     }
   }
 }
