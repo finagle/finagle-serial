c0801e644fd3adc2131a63199d4e92156249de6c
Travis Brown <tbrown@twitter.com>
2015-01-08 10:24:04 -0800

    A few small fixes to get scodec tests to run

--- serial-scodec/src/test/scala/io/github/finagle/serial/scodec/ScodecCodecSpec.scala
+++ serial-scodec/src/test/scala/io/github/finagle/serial/scodec/ScodecCodecSpec.scala
@@ -10,33 +10,29 @@ import _root_.scodec.codecs._
 import org.scalatest.{Matchers, FlatSpec}
 
 class ScodecCodecSpec extends FlatSpec with Matchers {
+  case class Point(x: Double, y: Double)
 
   "An Scodec Codec" should "work being used standalone" in {
-    import shapeless._
-    case class Point(x: Double, y: Double)
-    case class Point2(x: Int, y: Int)
-
     val pointCodec = (double :: double).as[Point]
-    val point2Codec = (int8 :: int8).as[Point2]
 
     val codec = toSerialCodec(pointCodec)
-    val d = toSerialCodec[Point2](point2Codec)
 
     val p = Point(3.0, 42.0)
     codec.roundTrip(p) shouldBe Return(p)
   }
 
+  case class Foo(x: Int)
+
   it should "work being used with Serial" in {
-    case class Foo(x: Int)
-    implicit val fooCodec = int8.as[Foo]
+    implicit val fooCodec = int8.hlist.as[Foo]
 
     val server = Serial[Foo, Foo].serve(
-      new InetSocketAddress(8123),
+      new InetSocketAddress(8124),
       new Service[Foo, Foo] {
         override def apply(x: Foo) = Future.value(Foo(x.x * x.x))
       }
     )
-    val service = Serial[Foo, Foo].newService("localhost:8123")
+    val service = Serial[Foo, Foo].newService("localhost:8124")
 
     Await.result(service(Foo(10))) shouldBe Foo(100)
     Await.ready(server.close())
